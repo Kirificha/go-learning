7da01e49a2f6ecf27f21f68b5ee4dc9bbe401344
Monlern <foroz2026@gmail.com>
2026-05-24 22:35:24 +0300

    4 variants of for loops in birdwatcher

--- exercism/birdwatcher/main.go
+++ exercism/birdwatcher/main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 	birdsPerDay := []int{2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1}
-	FixBirdCountLog(birdsPerDay)
 	TotalBirdCount(birdsPerDay)
 	BirdsInWeek(birdsPerDay, 2)
+	FixBirdCountLog(birdsPerDay)
+	FixBirdCountLog2(birdsPerDay)
+	FixBirdCountLog3(birdsPerDay)
+	FixBirdCountLog4(birdsPerDay)
 }
 
 // TotalBirdCount return the total bird count by summing
@@ -36,11 +39,30 @@ func BirdsInWeek(birdsPerDay []int, week int) int {
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
-func FixBirdCountLog(birdsPerDay []int) []int {
+func FixBirdCountLog(birdsPerDay []int) {
 	for i := 0; i < len(birdsPerDay); i += 2 {
 		birdsPerDay[i]++
 	}
 	fmt.Println(birdsPerDay)
-	return birdsPerDay
-	panic("Please implement the FixBirdCountLog() function")
+}
+
+func FixBirdCountLog2(birdsPerDay []int) {
+	for i := range birdsPerDay {
+		birdsPerDay[i*2]++
+	}
+	fmt.Println(birdsPerDay)
+}
+
+func FixBirdCountLog3(birdsPerDay []int) {
+	for key, value := range birdsPerDay {
+		birdsPerDay[key*2] = value + 1
+	}
+
+}
+
+func FixBirdCountLog4(birdsPerDay []int) {
+	for _, value := range birdsPerDay {
+
+		birdsPerDay[value] = value + 1
+	}
 }
