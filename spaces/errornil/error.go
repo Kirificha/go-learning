9083eac68b90461fffa4f7c5f48c71ea507655cf
Monlern <foroz2026@gmail.com>
2026-05-25 22:51:31 +0300

    spaces: errors with interfaces, nil-pointer

---
+++ spaces/errornil/error.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type MyError struct{}
+
+func (e *MyError) Error() string {
+	return "моя ошибка"
+}
+func validateLength(s string) error {
+	if len(s) < 3 {
+		return &MyError{}
+	}
+	return nil
+}
+
+func validateLengthBad(s string) error {
+	var myErr *MyError
+	if len(s) < 3 {
+		myErr = &MyError{}
+	}
+	return myErr
+}
+
+func main() {
+
+	err1 := validateLength("ab")
+	if err1 != nil {
+		fmt.Println(err1)
+	} else {
+		fmt.Println("Ошибки нет")
+	}
+
+	err2 := validateLength("abc")
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println("Ошибки нет")
+	}
+
+	err3 := validateLengthBad("abc")
+	if err3 != nil {
+		fmt.Println(err3)
+	} else {
+		fmt.Println("Ошибки нет")
+	}
+}
