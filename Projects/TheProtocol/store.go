bd913abbc2f91a8ed401c839bd983ad066f1c54d
Monlern <foroz2026@gmail.com>
2026-06-22 16:58:46 +0300

    TheProtocol: Backend methods

--- Projects/TheProtocol/store.go
+++ Projects/TheProtocol/store.go
@@ -19,5 +19,47 @@ type Store interface {
 	AddSession(s Session) error
 	ListSessionsByDate(date string) ([]Session, error)
 	ListSessionsByPeriod(firstDate string, lastDate string) ([]Session, error)
-	TotalMinutes() int
+	TotalMinutes() (int, error)
+}
+
+type MemoryStore struct {
+	Sessions []Session
+	ID       int
+}
+
+func (m *MemoryStore) AddSession(s Session) error {
+	m.ID++
+	m.Sessions = append(m.Sessions, s)
+	return nil
+}
+
+func (m *MemoryStore) ListSessionsByDate(date string) ([]Session, error) {
+	var goods []Session
+
+	for _, v := range m.Sessions {
+		if v.Date == date {
+			goods = append(goods, v)
+		}
+	}
+
+	return goods, nil
+}
+
+func (m *MemoryStore) ListSessionsByPeriod(firstDate string, lastDate string) ([]Session, error) {
+	var goods []Session
+
+	for _, v := range m.Sessions {
+		if v.Date >= firstDate && v.Date <= lastDate {
+			goods = append(goods, v)
+		}
+	}
+	return goods, nil
+}
+
+func (m *MemoryStore) TotalMinutes() (int, error) {
+	minutes := 0
+	for _, v := range m.Sessions {
+		minutes += v.Minutes
+	}
+	return minutes, nil
 }
